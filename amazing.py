77918d8fc7add92ed07fa480eb43de0c73c8e028
Arthur Moreira de Deus <arthur.md@parade.ai>
2022-02-10 10:41:02 -0300

    style: Adjust file indentation

--- amazing.py
+++ amazing.py
@@ -1,11 +1,11 @@
 class Amazing:
 
-  def __init__(self, name=None, description=None)
-    self.name = name
-    self.description = description
+    def __init__(self, name=None, description=None)
+        self.name = name
+        self.description = description
 
-  def do_amazing(self):
-    print(f"{self.name} is amazing!")
+    def do_amazing(self):
+        print(f"{self.name} is amazing!")
 
-  def do_description(self):
-    print(f"{self.name} is {self.description}!")
+    def do_description(self):
+        print(f"{self.name} is {self.description}!")
